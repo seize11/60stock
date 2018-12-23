e237242505a5318b6b195ad73339aea0ee35c61d
陈森 <CHENSEN001@pingan.com.cn>
2018-12-24 02:26:27 +0800

    awardResult静态页面

--- src/pages/AwardResult/index.js
+++ src/pages/AwardResult/index.js
@@ -1,74 +1,156 @@
 /* global window, document */
-import React, { Component } from "react";
-import PropTypes from "prop-types";
-import { connect } from "react-redux";
-import styles from "./Play.scss";
-import { GoBack } from "../../utils/history";
-import PearlRoad from "../../components/PearlRoad";
-import BigRoad from "../../components/BigRoad";
-import { decrypt } from "../../utils/info";
-import { getURLParamsObject } from "../../utils/url";
-import { getTableInitData, updateTableData } from "../../actions/table";
-import SocketUtils from "../../utils/socket";
-import Link from "../../components/Link";
-import Page from "../../components/Page";
-import PlayTopBar from "../../components/Play/PlayTopBar";
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import styles from './index.scss';
+import { GoBack } from '../../utils/history';
+import PearlRoad from '../../components/PearlRoad';
+import BigRoad from '../../components/BigRoad';
+import { decrypt } from '../../utils/info';
+import { getURLParamsObject } from '../../utils/url';
+import { getTableInitData, updateTableData } from '../../actions/table';
+import SocketUtils from '../../utils/socket';
+import Link from '../../components/Link';
+import Page from '../../components/Page';
+import MaskNav from '../../components/Play/MaskNav';
+import PlayTopBar from '../../components/Play/PlayTopBar';
+import { DatePicker, Icon } from 'antd-mobile';
 
 const propTypes = {};
 
 const defaultProps = {
-  leftContent: "北京赛车(PK10)"
+	leftContent: '北京赛车(PK10)',
 };
 
-class PlayPage extends Component {
-  constructor(props) {
-    super(props);
-  }
+class AwardResult extends Component {
+	constructor(props) {
+		super(props);
+		this.state = { isShow: false, date: new Date() };
+	}
 
-  componentDidMount() {}
+	componentDidMount() {}
 
-  componentWillUnmount() {
-    SocketUtils.unsubscribe();
-  }
+	componentWillUnmount() {
+		SocketUtils.unsubscribe();
+	}
+	handleIconClick() {
+		this.setState({ show: !this.state.show });
+	}
+	handleExit() {
+		this.setState({ show: false });
+	}
 
-  handleSocketData = res => {
-    // const { base,  {
-    //   return;
-    // }updateTableDataAction } = this.props;
-    // const { fastType, gameType } = getURLParamsObject();
-    // if (!base.wsKey)
-    // const data = JSON.parse(decrypt(res, base.wsKey));
-    // updateTableDataAction({
-    //   fastType,
-    //   gameType,
-    //   data
-    // });
-  };
-  renderTopBarContent = () => <div>存款</div>;
+	handleSocketData = res => {
+		// const { base,  {
+		//   return;
+		// }updateTableDataAction } = this.props;
+		// const { fastType, gameType } = getURLParamsObject();
+		// if (!base.wsKey)
+		// const data = JSON.parse(decrypt(res, base.wsKey));
+		// updateTableDataAction({
+		//   fastType,
+		//   gameType,
+		//   data
+		// });
+	};
+	renderTopBarContent = () => <div>存款</div>;
 
-  render() {
-    const { userName, balance, table, leftContent } = this.props;
+	render() {
+		const { userName, balance, table, leftContent } = this.props;
 
-    return (
-      <div className={styles.page}>
-        <PlayTopBar {...this.props} />
-      </div>
-    );
-  }
+		return (
+			<div className={styles.award_result}>
+				<PlayTopBar
+					{...this.props}
+					handleClick={() => {
+						this.handleIconClick();
+					}}
+				/>
+				<MaskNav
+					isShow={this.state.show}
+					handleExit={() => {
+						this.handleExit();
+					}}
+				/>
+				<div className={styles.award_result_header}>
+					<div className={styles.award_result_header_left}>北京赛车</div>
+					<div className={styles.award_result_header_right}>
+						<DatePicker
+							mode="date"
+							format="YYYY:MM:DD"
+							value={this.state.customChildValue}
+							onChange={v => this.setState({ customChildValue: v })}
+							extra="click to choose"
+						>
+							<span>
+								2018-12-16
+								<Icon type="right" />
+							</span>
+						</DatePicker>
+					</div>
+				</div>
+				<div className={styles.award_result_thead}>
+					<div className={styles.award_result_thead_td}>期数</div>
+					<div className={styles.award_result_thead_td}>时间</div>
+					<div className={styles.award_result_thead_td}>
+						<span>号码</span>{' '}
+					</div>
+					<div className={styles.award_result_thead_td}>
+						<span>大小</span>
+					</div>
+					<div className={styles.award_result_thead_td}>
+						<span>单双</span>
+					</div>
+					<div className={styles.award_result_thead_td}>
+						<span>冠亚/龙虎</span>
+					</div>
+				</div>
+				<div className={styles.award_result_tbody}>
+					{[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16].map(() => {
+						return (
+							<div className={styles.award_result_tbody_tr}>
+								<div className={styles.award_result_tbody_td}>123456789</div>
+								<div className={styles.award_result_tbody_td}>14:34</div>
+								<div className={styles.award_result_tbody_td}>
+									<span>大</span>
+								</div>
+								<div className={styles.award_result_tbody_td}>
+									<span>大</span>
+								</div>
+								<div className={styles.award_result_tbody_td}>
+									<span>大</span>
+								</div>
+								<div className={styles.award_result_tbody_td}>
+									<span>小</span>
+								</div>
+								<div className={styles.award_result_tbody_td}>
+									<span>小</span>
+								</div>
+								<div className={styles.award_result_tbody_td}>
+									<span>小</span>
+								</div>
+								<div className={styles.award_result_tbody_td}>
+									<span>小</span>
+								</div>
+								<div className={styles.award_result_tbody_td}>
+									<span>小</span>
+								</div>
+								<div className={styles.award_result_tbody_td}>
+									<span>大</span>
+								</div>
+								<div className={styles.award_result_tbody_td}>
+									<span>大</span>
+								</div>
+							</div>
+						);
+					})}
+				</div>
+			</div>
+		);
+	}
 }
 
-PlayPage.propTypes = propTypes;
-PlayPage.defaultProps = defaultProps;
+AwardResult.propTypes = propTypes;
+AwardResult.defaultProps = defaultProps;
 
-export default connect(
-  state => ({
-    base: state.base,
-    userName: state.user.name,
-    balance: state.account.balance,
-    table: state.table
-  }),
-  {
-    getTableInitDataAction: getTableInitData,
-    updateTableDataAction: updateTableData
-  }
-)(PlayPage);
+export default connect()(AwardResult);
--- src/pages/UnfinishedDetail/index.js
+++ src/pages/UnfinishedDetail/index.js
@@ -12,17 +12,91 @@ import { getTableInitData, updateTableData } from '../../actions/table';
 import SocketUtils from '../../utils/socket';
 import Link from '../../components/Link';
 import Page from '../../components/Page';
+import MaskNav from '../../components/Play/MaskNav';
 import PlayTopBar from '../../components/Play/PlayTopBar';
+import { SwipeAction, List, Flex } from 'antd-mobile';
 
 const propTypes = {};
 
 const defaultProps = {
 	leftContent: '北京赛车(PK10)',
+	nav: [
+		{
+			name: '时间',
+		},
+		{
+			name: '类型',
+		},
+		{
+			name: '玩法',
+		},
+		{
+			name: '下注',
+		},
+		{
+			name: '可赢',
+		},
+	],
+	list: [
+		{
+			time: '2018-12-16',
+			time_detail: '12:53:52',
+			name: '北京赛车(pk10)',
+			stamp: '86532',
+			level: '冠军',
+			level_num: '9.95',
+			chip_in: '20',
+			can_win: '179.0',
+		},
+		{
+			time: '2018-12-16',
+			time_detail: '12:53:52',
+			name: '北京赛车(pk10)',
+			stamp: '86532',
+			level: '冠军',
+			level_num: '9.95',
+			chip_in: '20',
+			can_win: '179.0',
+		},
+		{
+			time: '2018-12-16',
+			time_detail: '12:53:52',
+			name: '北京赛车(pk10)',
+			stamp: '86532',
+			level: '冠军',
+			level_num: '9.95',
+			chip_in: '20',
+			can_win: '179.0',
+		},
+		{
+			time: '2018-12-16',
+			time_detail: '12:53:52',
+			name: '北京赛车(pk10)',
+			stamp: '86532',
+			level: '冠军',
+			level_num: '9.95',
+			chip_in: '20',
+			can_win: '179.0',
+		},
+		{
+			time: '2018-12-16',
+			time_detail: '12:53:52',
+			name: '北京赛车(pk10)',
+			stamp: '86532',
+			level: '冠军',
+			level_num: '9.95',
+			chip_in: '20',
+			can_win: '179.0',
+		},
+	],
 };
 
 class UnfinishedDetail extends Component {
 	constructor(props) {
 		super(props);
+		this.state = {
+			isShow: false,
+		};
 	}
 
 	componentDidMount() {}
@@ -44,14 +118,105 @@ class UnfinishedDetail extends Component {
 		//   data
 		// });
 	};
+	handleIconClick() {
+		this.setState({ show: !this.state.show });
+	}
+	handleExit() {
+		this.setState({ show: false });
+	}
 	renderTopBarContent = () => <div>存款</div>;
 
 	render() {
 		const { userName, balance, table, leftContent } = this.props;
 
 		return (
-			<div className={styles.page}>
-				<PlayTopBar {...this.props} />
+			<div className={styles.unfinished_detail}>
+				<PlayTopBar
+					{...this.props}
+					handleClick={() => {
+						this.handleIconClick();
+					}}
+				/>
+				<MaskNav
+					isShow={this.state.show}
+					handleExit={() => {
+						this.handleExit();
+					}}
+				/>
+				<div className={styles.unfinished_detail_nav}>
+					{defaultProps.nav.map((item, index) => {
+						return <div className={styles.unfinished_detail_nav_item}>{item.name}</div>;
+					})}
+				</div>
+				<div className={styles.unfinished_detail_content}>
+					{defaultProps.list.map((item, index) => {
+						return (
+							<SwipeAction
+								style={{ backgroundColor: 'gray', height: '100%', display: 'flex', height: '1.12rem' }}
+								autoClose
+								right={[
+									{
+										text: '删除',
+										onPress: () => console.log('delete'),
+										style: {
+											backgroundColor: '#F4333C',
+											color: 'white',
+											fontSize: '0.32rem',
+											width: '5rem',
+											textAlign: 'center',
+										},
+									},
+									{
+										text: '取消',
+										onPress: () => console.log('delete'),
+										style: {
+											backgroundColor: '#cccccc',
+											color: 'white',
+											fontSize: '0.32rem',
+											width: '5rem',
+											textAlign: 'center',
+										},
+									},
+								]}
+								onOpen={() => console.log('global open')}
+								onClose={() => console.log('global close')}
+							>
+								<List.Item
+									arrow={false}
+									onClick={() => console.log('List.Item clicked!')}
+									style={{ display: 'flex', height: '1.1rem' }}
+								>
+									<div className={styles.unfinished_detail_content_item}>
+										<div className={styles.unfinished_detail_content_item_cell}>
+											{item.time}
+											<br />
+											{item.time_detail}
+										</div>
+										<div className={styles.unfinished_detail_content_item_cell}>
+											{item.name}
+											<br />第<span>{item.stamp}</span>期
+										</div>
+										<div className={styles.unfinished_detail_content_item_cell}>
+											{item.level}
+											<br />@<span>{item.level_num}</span>
+										</div>
+										<div className={styles.unfinished_detail_content_item_cell}>{item.chip_in}</div>
+										<div className={styles.unfinished_detail_content_item_cell}>{item.can_win}</div>
+									</div>
+								</List.Item>
+							</SwipeAction>
+						);
+					})}
+				</div>
+				<div className={styles.unfinished_detail_footer}>
+					<ul>
+						<li>注数：7</li>
+						<li>下注金额：140</li>
+						<li>
+							结果(总计)：<span>9876543.00</span>
+						</li>
+					</ul>
+				</div>
 			</div>
 		);
 	}
