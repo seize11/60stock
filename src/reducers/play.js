53fc935f6699f40c0c8bde79a698d0027384ce4c
陈森 <CHENSEN001@pingan.com.cn>
2019-01-03 22:47:50 +0800

    ertui

--- src/reducers/play.js
+++ src/reducers/play.js
@@ -1,11 +1,14 @@
-import actionTypes from "../constants/actionTypes";
+import actionTypes from '../constants/actionTypes';
 
 const initialState = {
-<<<<<<< HEAD
 	selectIds: [],
 	totalList: [],
 	unfinishedDetail: [],
 	awardResult: [],
+	selectIds: [],
+	totalList: [],
+	lottery_info: {},
+	unfinishedDetail: [],
 };
 
 export default function playProps(state = initialState, action = {}) {
@@ -45,54 +48,21 @@ export default function playProps(state = initialState, action = {}) {
 		case actionTypes.DELETE_UNFINISHED_DETAIL_SUCCESS:
 			console.log(action);
 			return { ...state, deleteResult: payload };
+		case actionTypes.GO_BET_REQUEST:
+			return { ...state };
+		// loadingState: LOADING_STATE.BEGIN
+		case actionTypes.GO_BET_SUCCESS:
+			console.log(payload);
+			return { ...state, lottery_info: payload };
+		case actionTypes.GET_LOTTERY_INFO_REQUEST:
+			return { ...state };
+		// loadingState: LOADING_STATE.BEGIN
+		case actionTypes.GET_LOTTERY_INFO_SUCCESS:
+			console.log(payload);
+			return { ...state, lottery_info: payload };
 		default:
 	}
 	return state;
-=======
-  selectIds: [],
-  totalList: [],
-  lottery_info: {},
-  unfinishedDetail: []
-};
-
-export default function playProps(state = initialState, action = {}) {
-  const { type, payload } = action;
-  console.log(payload);
-  switch (type) {
-    //======CHANGE_SELECT_POPS
-    case actionTypes.CHANGE_SELECT_POPS:
-      return { ...state, selectIds: payload };
-    //========GET_TOTAL_LIST_REQUEST
-    case actionTypes.GET_TOTAL_LIST_REQUEST:
-      return { ...state };
-    // loadingState: LOADING_STATE.BEGIN
-    case actionTypes.GET_TOTAL_LIST_SUCCESS:
-      console.log(payload);
-      return { ...state, totalList: payload };
-    case actionTypes.GET_LOTTERY_INFO_REQUEST:
-      return { ...state };
-    // loadingState: LOADING_STATE.BEGIN
-    case actionTypes.GET_LOTTERY_INFO_SUCCESS:
-      console.log(payload);
-      return { ...state, lottery_info: payload };
-    case actionTypes.GO_BET_REQUEST:
-      return { ...state };
-    // loadingState: LOADING_STATE.BEGIN
-    case actionTypes.GO_BET_SUCCESS:
-      console.log(payload);
-      return { ...state, lottery_info: payload };
-    // loadingState: LOADING_STATE.SUCCESS
-    //========GET_TOTAL_LIST_REQUEST
-    case actionTypes.GET_UNFINISHED_DETAIL_REQUEST:
-      return { ...state };
-    // loadingState: LOADING_STATE.BEGIN
-    case actionTypes.GET_UNFINISHED_DETAIL_SUCCESS:
-      return { ...state, unfinishedDetail: payload };
-    // loadingState: LOADING_STATE.SUCCESS
-    default:
-  }
-  return state;
->>>>>>> df4fec1e92582cf0653a4ab1d93a51f0d0ca6e1c
 }
 
 // export default function GetTotalList(state = initialState, action = {}) {
